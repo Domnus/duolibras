1f7f36451d1ce5e6ce74828847eaa6cd9696e356
Bento Carlos <bcsilva49@gmail.com>
2023-09-02 17:10:27 -0300

    Fix back button

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Pressable } from 'react-native';
+import { Pressable, Text } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import MainScreen from './app/home/index';
