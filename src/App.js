a3a9b6c19cd093c5088c467ae7497668b53ee9b4
Seitii <hugoseite@icloud.com>
2023-09-30 14:34:33 -0300

    Excluindo arquivos desnecessário

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Header from './components/Header'
-//import MainLogin from './components/Home';
-//import Home from './components/Home'
+import MainLogin from './components/Home';
+import Home from './components/Home'
 
 function App() {
   return (
-    //<MainLogin />,
+    <MainLogin />,
     <Header />
   );
 }
